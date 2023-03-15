e4b4567e1b7b3f11da33c419440b14f483270dbc
garrettsh09 <sharpeg68@gmail.com>
2023-03-15 00:54:34 -0400

    Player Charecter class

---
+++ player.js
@@ -0,0 +1,58 @@
+export default class player{
+    constructor(gameWidth, gameHeight){ 
+        this.gameWidth = gameWidth
+
+        this.width = 10;
+        this.height = 10;
+
+        this.position = {
+            x: gameWidth/2 - this.width/2,
+            y: gameHeight/2 - this.height-5
+        };
+        this.maxspeed = 5;
+        this.speed = 0;
+
+        this.states = {
+            'holdingGarbage': document.getElementById(), //Player-Garabge image
+            'holdingCompost': document.getElementById(), //Player-Compost image
+            'holdingRecyler': document.getElementById(), //Player-Recylcer image
+            'holdingPaperRecyler': document.getElementById() //Player-PaperRecylcer image
+        }
+        this.state = 'holdingGarbage'
+        this.image = this.states[this.state]
+
+    }
+
+    moveLeft(){
+        this.speed = -this.maxspeed;
+    }
+
+    moveRight(){
+        this.speed = this.maxspeed;
+    }
+
+    stop(){
+        this.speed = 0;
+    }
+
+    draw(ctx){
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update(deltatime){
+        if (!deltatime) return;
+
+        this.image = this.states[this.state]
+
+        this.position.x  += this.speed;
+        
+        if (this.position.x < 0){
+            this.positition.x = 0;
+        }
+
+        if (this.position.x + this.width > gameWidth){
+            this.position.x = gameWidth - this.width;
+        }
+    }
+
+}
