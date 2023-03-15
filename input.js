bb5f6b89bcfdf052dd96cfd456230ddde303fc21
weiqixu05 <weiqixu2005@icloud.com>
2023-03-15 19:42:09 -0400

    changed event.keyCode to event.key

--- input.js
+++ input.js
@@ -1,38 +1,38 @@
 export default class input{
     constructor(){
         document.addEventListener('keydown', (event) => {
-            alert(event.keyCode);
-            switch (event.keyCode){
-                case 37: // left arrow key
+            alert(event.key);
+            switch (event.key){
+                case 'ArrowLeft': // left arrow key
                     player.moveLeft();
                     break;
-                case 39: // right arrow key
+                case 'ArrowRight': // right arrow key
                     player.moveRight();
                     break;
-                case 49: // 1 key
+                case '1': // 1 key
                     this.state = 'holdingGarbage';
                     break;
-                case 50: // 2 key
+                case '2': // 2 key
                     this.state = 'holdingCompost';
                     break;
-                case 51: // 3 key
+                case '3': // 3 key
                     this.state = 'holdingRecycler';
                     break;
-                case 52: // 4 key
+                case '4': // 4 key
                     this.state = 'holdingPaperRecycler';
                     break;
             }
         });
 
         document.addEventListener('keyup', (event) => {
-            alert(event.keyCode);
-            switch (event.keyCode){
-                case 37: // left arrow key unheld
+            alert(event.key);
+            switch (event.key){
+                case 'ArrowLeft': // left arrow key unheld
                     if(player.speed < 0){
                         player.stop();
                     }
                     break;
-                case 39: // right arrow key unheld
+                case 'ArrowRight': // right arrow key unheld
                     if (player.speed > 0){
                         player.stop();
                     }
