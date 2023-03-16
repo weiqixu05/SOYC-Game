593fca628b78c16101ca559bb4e778c5f7df35d8
garrettsh09 <sharpeg68@gmail.com>
2023-03-16 18:02:33 -0400

    itemfunc

---
+++ src/itemfunc.js
@@ -0,0 +1,16 @@
+import compostitem from "./CompostItem";
+import recylceritem from "./RecyclerItem";
+import garbageitem from "./GarbageItem";
+import paperitem from "./PaperItem";
+
+export default class ItemFunc{
+    constructor(){
+        this.height = 10
+        this.width = 10
+    }
+
+
+    draw(ctx){
+        
+    }
+}
