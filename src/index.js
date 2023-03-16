8c608bcdf8d3ac8b54f79128fd8d20255537deb1
garrettsh09 <sharpeg68@gmail.com>
2023-03-16 14:08:34 -0400

    Added Gameloop

--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ import Bin from 'src/bin';
 let canvas=document.getElementById("gameScreen");
 let ctx=canvas.getContext("2d"); //context is what we use to draw on canvas
 
-const GAME_WIDTH = 1920
-const GAME_HEIGHT = 1080
+const GAME_WIDTH = 1920;
+const GAME_HEIGHT = 1080;
 
 ctx.clearRect(0,0,800,600);
 
-let player = new Player(GAME_WIDTH, GAME_HEIGHT)
+let player = new Player(GAME_WIDTH, GAME_HEIGHT);
 
-player.draw(ctx)
+player.draw(ctx);
+
+let lastTime = 0;
+
+
+
+function gameloop(timestamp){
+    let deltatime = timestamp - lastTime;
+    lastTime = timestamp
+
+
+    ctx.clearRect(0, 0, 1920, 1080);
+    player.update();
+    player.draw(ctx);
+
+    requestAnimationFrame(gameloop);
+}
+
+gameloop();
