0ed292b0dcd6820235afb2c26ee6b26202cc1233
weiqixu05 <weiqixu2005@icloud.com>
2023-03-17 21:00:48 -0400

    fixed importing files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-
-import Bin from 'src/bin';
-
+import Bin from "./bin.js"
 
 let canvas=document.getElementById("gameScreen");
 let ctx=canvas.getContext("2d"); //context is what we use to draw on canvas
@@ -8,9 +6,6 @@ let ctx=canvas.getContext("2d"); //context is what we use to draw on canvas
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-ctx.clearRect(0,0,800,600);
 
-ctx.clearRect(0,0,800,600);
-
-let bin= new Bin(GAME_WIDTH, GAME_HEIGHT);
+let bin = new Bin(GAME_WIDTH, GAME_HEIGHT);
 bin.draw(ctx);
