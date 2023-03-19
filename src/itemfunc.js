5022c74b052694792018fcdb0ecbdf76af44d6f5
weiqixu05 <weiqixu2005@icloud.com>
2023-03-19 17:17:31 -0400

    added randomized falling object colour

--- src/itemfunc.js
+++ src/itemfunc.js
@@ -1,7 +1,7 @@
 export default class ItemFunc{
     constructor(){
-        this.height = 10;
-        this.width = 10;
+        this.height = 25;
+        this.width = 25;
         this.speed = 5;
         this.count=1;
         this.hearts=3;
@@ -10,11 +10,19 @@ export default class ItemFunc{
             x: Math.floor(Math.random() * 900),
             y: 0
         };
+        this.state={
+            "1":document.getElementById("black item"),
+            "2":document.getElementById("green item"),
+            "3":document.getElementById("purple item"),
+            "4":document.getElementById("red item")
+        }
+        this.currentState=(Math.floor(Math.random() * 4) + 1).toString();
+        this.img=this.state[this.currentState];
     }
     
     draw(ctx){
-        ctx.fillStyle='0ff';
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        this.img=this.state[this.currentState];
+        ctx.drawImage(this.img, this.position.x, this.position.y);
     }
     drawUi(ctx){
         ctx.font = "25px serif";
@@ -23,6 +31,7 @@ export default class ItemFunc{
    respawn(){
         this.position.y = 0;
         this.position.x = Math.floor(Math.random() * 900);
+        this.currentState=(Math.floor(Math.random() * 4) + 1).toString();
     }
 
     collide(player){
@@ -35,15 +44,17 @@ export default class ItemFunc{
         if (!deltatime) return;
 
         this.position.y  += this.speed;
-
-        if(this.position.y >= 600){this.respawn();}
         
-       if(this.collide(player)){
-         this.respawn();
-         this.score++;
+       if(this.collide(player)&&player.chosenState===this.currentState){
+            this.respawn();
+            this.score++;
        }
-            
-  
+       
+       else if(this.collide(player)&&player.chosenState!==this.currentState){
+            this.respawn();
+            this.hearts--;
+       }
+       
         else if (this.position.y >= player.position.y+player.height){
             this.hearts--;
             this.respawn();
