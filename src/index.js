61a3363fc4564bcb72298941fb8c335a3f6e66ab
weiqixu05 <weiqixu2005@icloud.com>
2023-03-25 23:10:22 -0400

    got rid of item 3

--- src/index.js
+++ src/index.js
@@ -18,8 +18,6 @@ let item = new ItemFunc();
 item.draw(ctx);
 let item2 = new ItemFunc();
 item2.position.y = item.position.y-(GAME_HEIGHT/2);
-let item3 = new ItemFunc();
-item3.position.y = -150
 
 let lastTime=0;
 
@@ -39,15 +37,12 @@ function gameLoop(timestamp){
         //resetting everything
         item.score=0;
         item2.score=0;
-        item3.score=0;
         item.hearts=3;
         item2.hearts=3;
-        item3.hearts=3;
         score=0;
         item.position.y=0;
         item.position.x=Math.floor(Math.random() * 900);
         item2.position.y=item.position.y-300;
-        item3.position.y=-150;
     }
     else{
     let deltaTime=timestamp-lastTime;
@@ -62,8 +57,8 @@ function gameLoop(timestamp){
     player.update(deltaTime);
     player.draw(ctx);
     //updates item 
-    score=item.score+item2.score+item3.score;
-    hearts=3-(9-item.hearts-item2.hearts-item3.hearts);
+    score=item.score+item2.score;
+    hearts=3-(9-item.hearts-item2.hearts);
     item.draw(ctx);
     item.drawUi(ctx);
     item.update(deltaTime, player);
@@ -78,10 +73,6 @@ function gameLoop(timestamp){
         item2.draw(ctx);
         item2.update(deltaTime, player);
     }
-    if (item.score >= 20){
-        item3.draw(ctx);
-        item3.update(deltaTime, player);
-    } 
 }
 
 }
